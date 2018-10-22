8a5bbc958dfc5a21414e86070c3444e4259f0dba
designtech44 <designtech44@gmail.com>
2018-10-21 21:24:16 -0400

    updates

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -9,28 +9,38 @@ use Symfony\Component\HttpFoundation\Response;
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/") 
+     * @Route("/", name="app_homepage") 
      */
     public function homepage()
     {
-        return new Response('OMG! My first page already! Wooo!');
+        return $this->render('article/homepage.html.twig');
     }
 
     /**
-     * @Route("/news/{slug}") 
+     * @Route("/news/{slug}", name="article_show") 
      */
-     public function show($slug)
-     {
+    public function show($slug)
+    {
         $comments = [
             'I ate a normal rock once. It did NOT taste like bacon!',
             'Woohoo! I\'m going on an all-asteroid diet!',
             'I like bacon too! Buy some from my site! bakinsomebacon.com',
         ];
         dump($slug, $this);
-        
-         return $this->render('article/show.html.twig', [
+
+        return $this->render('article/show.html.twig', [
               'title'=> ucwords(str_replace('-', ' ', $slug)),
+              'slug' => $slug,
               'comments' => $comments,
          ]);
+     }
+     /**
+      * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"}) 
+      */
+     public function toggleArticleHeart($slug)
+     {
+         // TODO - actually heart/unheart the article!
+
+         return new JsonResponse(['hearts' => rand(5, 100)]);
      }
 }
